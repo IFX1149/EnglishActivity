854e59454aacda61d33662b45b9e255f092549af
IFX1149 <ifx1149@gmail.com>
2024-07-20 12:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+function showSlide(n) {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+prevBtn.addEventListener('click', () => showSlide(currentSlide - 1));
+nextBtn.addEventListener('click', () => showSlide(currentSlide + 1));
+
+// Show the first slide initially
+showSlide(0);
